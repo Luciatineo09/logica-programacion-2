9b3a297d42ddcca2f0840a40b2b047f5252876ac
Luciatineo09 <amaliatineo09@hotmail.com>
2025-01-22 14:12:43 -0500

    ejercicio terminado en consola

--- temperatura.js
+++ temperatura.js
@@ -1,23 +1,29 @@
 const prompt = require('prompt-sync')();
+centinela = true;
 
-console.log("¡Bienvenido! - sistema de conversión de temperatura");
-temperature = prompt("Por favor ingrese la temperatura en grados Celsius: ");
+while(centinela){
 
+    console.log("¡Bienvenido! - sistema de conversión de temperatura");
+    temperature = prompt("Por favor ingrese la temperatura en grados Celsius: ");
+    Number(temperature);
 
-if (temperature === "") {
+    if (temperature === "") {
+        console.log("ingresaste un valor vacío");
 
-    console.log("ingresaste un valor vacío");
-}
-else{
-    if (typeof(temperature)==Number) {
-        // Convertir la temperatura ingresada de grados Celsius a grados Fahrenheit y Kelvin
-        Fahrenheit = ((temperature *9)/5)+32
-        Kelvin = (temperature+273.15)
-        console.log(`La temperatura convertida a grados Fahrenheit es de: ${Fahrenheit}`);
-        console.log(`La temperatura convertida a grados kELVIN es de: ${Kelvin}`);
     }
     else{
-        console.log("Ingresa un valor numerico");
+        if (isNaN(temperature)) {
+            console.log("Ingresa un valor numerico");
+        }
+        else{
+            // Convertir la temperatura ingresada de grados Celsius a grados Fahrenheit y Kelvin
+            Fahrenheit = ((temperature *9)/5)+32
+            Kelvin = temperature + 273.15
+            console.log(`La temperatura convertida a grados Fahrenheit es de: ${Fahrenheit}`);
+            console.log(`La temperatura convertida a grados kELVIN es de: ${Kelvin}`);
+            centinela = false;
+
+        }
+
     }
-    
 }
